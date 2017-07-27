cfcccdc710d92c91a30936f85dfb01e10777a3bb
wow5468 <wow5468@naver.com>
2017-07-28 01:52:21 +0900

    Added some smartbookshelf api

--- bookshelf/views.py
+++ bookshelf/views.py
@@ -3,7 +3,7 @@ import requests
 from rest_framework import status, permissions
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
-from .models import MyShelf
+from .models import MyShelf, FcmToken
 from django.forms.models import model_to_dict
 
 from xmljson import parker
@@ -26,3 +26,21 @@ def api_saveSelf(request):
         mySelf.save()
         return Response(model_to_dict(mySelf))
     return Response(model_to_dict(mySelfs[0]))
+
+@api_view(['GET'])
+@permission_classes((permissions.IsAuthenticated,))
+def api_saveToken(request):
+    tokenType = int(request.GET.get("type"))
+    fcmToken = FcmToken.objects.filter(tokenType=tokenType, shelf__user=request.user)
+    if len(fcmToken) == 0:
+        fcmToken = FcmToken()
+        fcmToken.shelf = MyShelf.objects.get(user=request.user)
+        fcmToken.token = request.GET.get("token")
+        fcmToken.tokenType = tokenType
+        fcmToken.save()
+        return Response(model_to_dict(fcmToken))
+    else:
+        fcmToken = fcmToken[0]
+        fcmToken.token = request.GET.get("token")
+        fcmToken.save()
+        return Response(model_to_dict(fcmToken))
